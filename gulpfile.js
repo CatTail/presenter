ba721c7c597ecd9dbe0e411914b3f282d04e10bf
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2014-10-23 18:55:50 +0800

    Add gulp build to compress files

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var bowerDir = 'bower_components/';
+var sources = [
+    'bower_components/firebase/firebase.js',
+    'bower_components/qrcodejs/qrcode.min.js',
+    'bower_components/quojs/quo.standalone.js',
+    'index.js'
+];
+
+gulp.task('build', function() {
+    gulp.src(sources)
+        .pipe(plugins.concat('presenter.min.js'))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest('dist'))
+});
