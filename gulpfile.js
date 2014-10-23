47eff58c38891b258a89253ebe59489c850e00e5
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2014-10-23 22:15:51 +0800

    Auto compile in development

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,21 @@ var bowerDir = 'bower_components/';
 var sources = [
     'bower_components/firebase/firebase.js',
     'bower_components/qrcodejs/qrcode.min.js',
-    //'bower_components/quojs/quo.standalone.js',
-    'quo.js',
+    'bower_components/hammerjs/hammer.min.js',
     'index.js'
 ];
+
+gulp.task('dev', function() {
+    gulp.watch(sources, function() {
+        gulp.src(sources)
+            .pipe(plugins.concat('presenter.min.js'))
+            .pipe(gulp.dest('dist'));
+    });
+});
 
 gulp.task('build', function() {
     gulp.src(sources)
         .pipe(plugins.concat('presenter.min.js'))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
 });
