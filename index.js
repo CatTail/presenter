aa04ee589e594f10546c1aeaa8707085eb3dd54c
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2018-12-26 14:37:04 +0800

    remove hammerjs deps

--- index.js
+++ index.js
@@ -1,58 +1,50 @@
-function Presenter (password) {
+function Presenter () {
   var self = this
+
+  // bind methods with this
+  Object.getOwnPropertyNames(Object.getPrototypeOf(self))
+    .filter(function (name) { return typeof self[name] === 'function' })
+    .filter(function (name) { return name !== 'constructor' })
+    .forEach(function (name) { self[name] = self[name].bind(self) })
 
   // remove hash, conver to base64
   self.appName = window.btoa(window.location.href.slice(0, window.location.href.indexOf(window.location.hash)))
   // remove invalid characters
   self.namespace = self.appName.replace(/[^a-zA-Z0-9]+/g, '')
-  self.peers = {}
   self.peerId = self.namespace + ' ' + Math.random().toString().slice(2)
-  self.peer = new Peer(self.peerId, { host: 'peerjs.now.sh', port: 443, secure: true })
+  self.peers = {}
+  self.peer = null
+  self.touchCount = 0
+  self.timeoutId = null
 
+  self.register()
+  self.subscribe()
+
+  document.addEventListener('keypress', self.onKeyPress)
+  document.addEventListener('mousedown', self.onTouchStart)
+  document.addEventListener('touchstart', self.onTouchStart)
+}
+
+Presenter.prototype.register = function () {
+  var self = this
+
+  self.peer = new Peer(self.peerId, { host: 'peerjs.now.sh', port: 443, secure: true })
   self.peer.on('connection', function (conn) {
     self.peers[conn.peer] = conn
     conn.on('data', self.onDatahandler)
-  })
-
-  self.createQRCode()
-  self.subscribe()
-  self.listenTouchEvent()
-}
-
-Presenter.prototype.listenTouchEvent = function () {
-  var self = this
-  var hammer = new Hammer(document.body)
-
-  document.addEventListener('keypress', function (event) {
-    if (event.key === 'q') {
-      self.toggleQRCode()
-    }
-  }, false)
-
-  hammer.on('doubletap', function () {
-    hammer.on('tap', togglePublish)
-
-    setTimeout(function () {
-      hammer.off('tap', togglePublish)
-    }, 1000)
-
-    function togglePublish () {
-      hammer.off('tap', togglePublish)
-      self.publish()
-    }
   })
 }
 
 Presenter.prototype.publish = function () {
   window.alert('publish mode on')
-  window.onhashchange = this.onHashChange.bind(this)
+  window.onhashchange = this.onHashChange
 }
 
 Presenter.prototype.subscribe = function () {
   var self = this
   this.peer.listAllPeers(function (peers) {
     peers
-      // filter peers in the same namespace
+    // filter peers in the same namespace
       .filter(function (id) { return id !== self.peerId && id.split(' ')[0] === self.namespace })
       .forEach(function (id) {
         if (!self.peers[id]) {
@@ -62,6 +54,32 @@ Presenter.prototype.subscribe = function () {
         }
       })
   })
+}
+
+Presenter.prototype.onTouchStart = function (event) {
+  var self = this
+
+  if (self.timeoutId) {
+    clearTimeout(self.timeoutId)
+    self.timeoutId = null
+  }
+
+  self.touchCount = self.touchCount + 1
+  if (self.touchCount === 6) {
+    document.removeEventListener('mousedown', self.onTouchStart)
+    document.removeEventListener('touchstart', self.onTouchStart)
+    self.publish()
+  } else {
+    self.timeoutId = setTimeout(function () {
+      self.touchCount = 0
+    }, 500)
+  }
+}
+
+Presenter.prototype.onKeyPress = function (event) {
+  if (event.key === 'q') {
+    this.toggleQRCode()
+  }
 }
 
 Presenter.prototype.onHashChange = function () {
@@ -75,18 +93,19 @@ Presenter.prototype.onDatahandler = function (data) {
   window.location.href = data
 }
 
-Presenter.prototype.createQRCode = function () {
-  this.elCode = document.createElement('div')
-  this.elCode.style.display = 'none'
-  this.elCode.style.position = 'absolute'
-  this.elCode.style.top = '0'
-  this.elCode.style.right = '0'
-  this.elCode.style.zIndex = '1000'
-  document.body.appendChild(this.elCode)
-  return new QRCode(this.elCode, window.location.href)
-}
-
 Presenter.prototype.toggleQRCode = function () {
+  if (!this.elCode) {
+    this.elCode = document.createElement('div')
+    this.elCode.style.display = 'none'
+    this.elCode.style.position = 'absolute'
+    this.elCode.style.top = '0'
+    this.elCode.style.right = '0'
+    this.elCode.style.zIndex = '1000'
+    document.body.appendChild(this.elCode)
+    // eslint-disable-next-line
+    new QRCode(this.elCode, window.location.href)
+  }
+
   if (this.elCode.style.display === 'none') {
     this.elCode.style.display = 'block'
   } else {
@@ -95,4 +114,4 @@ Presenter.prototype.toggleQRCode = function () {
 }
 
 // eslint-disable-next-line
-new Presenter('god')
+new Presenter()
